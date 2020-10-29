36461a5d061f9500fdd6fc65f8fee29aadf7e0aa
Vanh1112 <49738178+Vanh1112@users.noreply.github.com>
2020-10-29 11:55:45 +0700

    add code

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="VN_text_preprocess",
+    version="0.0.1",
+    author="Vanhnee",
+    author_email="phamvananh11150348@gmail.com",
+    description="Sort description",
+    long_description="Full description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
